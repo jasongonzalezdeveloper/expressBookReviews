f50c8562c3dc560ef817e219d1c9814915d054cf
Jason F. Gonzalez Garcia <jason.gonzalez.developer@gmail.com>
2025-07-23 17:32:39 -0600

    task_2 get book by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,9 +17,13 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const id = req.params.isbn;
+  //Check if the book exists in the database 
+  if (!books[id]) {
+    return res.status(404).json({message: "Book not found"});
+  }
+  return res.status(200).send(JSON.stringify(books[id],null,4));
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
