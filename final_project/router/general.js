7e0afb33dcb8192430e9123dd8b29c3f181f765e
Jason F. Gonzalez Garcia <jason.gonzalez.developer@gmail.com>
2025-07-23 17:38:32 -0600

    tesk_4 get books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,8 +38,13 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+
+  const filteredBooks = Object.values(books).filter(book => book.title.toLowerCase() === title.toLowerCase());
+  if (filteredBooks.length === 0) {
+    return res.status(404).json({message: "No books found for this author"});
+  }
+  return res.status(200).send(JSON.stringify(filteredBooks,null,4));
 });
 
 //  Get book review
